03c527b9fb192f65f30e8961539ac138a184e386
kenyklam330 <kenyklam330@gmail.com>
2026-02-13 11:14:36 +0000

    Set base path in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/life-in-the-uk-test-pern-app/',
   server: {
     port: 5173,
     proxy: {
